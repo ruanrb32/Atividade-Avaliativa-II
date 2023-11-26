2e0f9c830c590c55c3fb71a32372d50aa9375168
ruanrb32 <ruan.rebelo@gmail.com>
2023-11-25 23:24:30 -0400

    Update # Questao10.py

--- # Questao10.py
+++ # Questao10.py
@@ -4,14 +4,14 @@
 
 
 
-# Importando bibliotecas necessárias
+# Importando as bibliotecas necessárias
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# Carregando o conjunto de dados Iris
+# Carregando o conjunto de Dados Iris
 from sklearn.datasets import load_iris
 iris = load_iris()
 X = iris.data
